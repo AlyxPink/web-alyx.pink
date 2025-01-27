b02b0f010d799445b833d2f4f904eaf80599e6e5
Alyx <github@alyx.pink>
2025-01-27 12:02:55 +0100

    Add FR translation, set domain, add projects to navbar

---
+++ src/i18n/languages/fr.ts
@@ -0,0 +1,39 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const fr: Translation = {
+  [Key.home]: 'Accueil',
+  [Key.about]: 'À propos',
+  [Key.archive]: 'Archives',
+  [Key.projects]: 'Projets',
+  [Key.search]: 'Rechercher',
+
+  [Key.tags]: 'Tags',
+  [Key.categories]: 'Catégories',
+  [Key.recentPosts]: 'Derniers articles',
+
+  [Key.comments]: 'Commentaires',
+
+  [Key.untitled]: 'Sans titre',
+  [Key.uncategorized]: 'Non catégorisé',
+  [Key.noTags]: 'Pas de tags',
+
+  [Key.wordCount]: 'mot',
+  [Key.wordsCount]: 'mots',
+  [Key.minuteCount]: 'minute',
+  [Key.minutesCount]: 'minutes',
+  [Key.postCount]: 'article',
+  [Key.postsCount]: 'articles',
+
+  [Key.themeColor]: 'Couleur du thème',
+
+  [Key.lightMode]: 'Clair',
+  [Key.darkMode]: 'Sombre',
+  [Key.systemMode]: 'Système',
+
+  [Key.more]: 'Plus',
+
+  [Key.author]: 'Auteur',
+  [Key.publishedAt]: 'Publié le',
+  [Key.license]: 'Licence',
+}
