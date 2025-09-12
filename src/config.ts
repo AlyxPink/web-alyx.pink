f0759c715bdd6372b77f905edebd0ce80fabf2b5
Alyx <git@alyx.pink>
2025-09-12 05:58:06 +0200

    refactor(ui): replace demo banner but one i finally found and love

--- src/config.ts
+++ src/config.ts
@@ -3,39 +3,40 @@ import type {
   NavBarConfig,
   ProfileConfig,
   SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+} from "./types/config";
+import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: 'Alyx.pink',
-  subtitle: '',
-  lang: 'en',         // 'en', 'fr'
+  title: "Alyx.pink",
+  subtitle: "",
+  lang: "en", // 'en', 'fr'
   themeColor: {
-    hue: 340,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 340, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: "assets/images/banner.svg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
-    }
+      enable: true, // Display the credit text of the banner image
+      text: 'A girl walks towards the night sky by "ands//picociko"',
+      url: "https://unsplash.com/illustrations/a-girl-walks-towards-the-night-sky-Gu_hYiE3v7g", // (Optional) URL link to the original artwork or artist's page
+    },
   },
   toc: {
-    enable: true,           // Display the table of contents on the right side of the post
-    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+    enable: true, // Display the table of contents on the right side of the post
+    depth: 2, // Maximum heading depth to show in the table, from 1 to 3
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-  ]
-}
+  ],
+};
 
 export const navBarConfig: NavBarConfig = {
   links: [
@@ -44,41 +45,41 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     LinkPreset.Projects,
   ],
-}
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Alyx',
-  bio: 'Professional engineer for 11 years. Self-taught. Ex-Senior Infra Engineer @ Docker Inc. Love Go and AI/LLM stuff.',
+  avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: "Alyx",
+  bio: "Professional engineer for 11 years. Self-taught. Ex-Senior Infra Engineer @ Docker Inc. Love Go and AI/LLM stuff.",
   links: [
     // Visit https://icones.js.org/ for icon codes
     // You will need to install the corresponding icon set if it's not already included
     // `pnpm add @iconify-json/<icon-set-name>`
     {
-      name: 'Bluesky',
-      icon: 'fa6-brands:bluesky',
-      url: 'https://bsky.app/profile/alyx.pink',
+      name: "Bluesky",
+      icon: "fa6-brands:bluesky",
+      url: "https://bsky.app/profile/alyx.pink",
     },
     {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/AlyxPink',
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/AlyxPink",
     },
     {
-      name: 'LinkedIn',
-      icon: 'fa6-brands:linkedin-in',
-      url: 'https://www.linkedin.com/in/alyx-pink/',
+      name: "LinkedIn",
+      icon: "fa6-brands:linkedin-in",
+      url: "https://www.linkedin.com/in/alyx-pink/",
     },
     {
-      name: 'Email',
-      icon: 'fa6-solid:envelope',
-      url: 'mailto:blog@alyx.pink',
+      name: "Email",
+      icon: "fa6-solid:envelope",
+      url: "mailto:blog@alyx.pink",
     },
   ],
-}
+};
 
 export const licenseConfig: LicenseConfig = {
   enable: true,
-  name: 'CC BY-NC-SA 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-}
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
